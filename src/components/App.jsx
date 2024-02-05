b0a1d92ec5d6a3da046464fbeba1a823ca24fe5f
mswitas <mswitas@gmail.com>
2024-02-05 15:38:24 +0100

    add stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import { Profile } from "./Profile";
 import user from "data/user.json";
+import { Statistics } from "./Statistics";
+import stats from "data/stats.json";
 
 export const App = () => {
   return (
     <>
-      <section>
-        <Profile
-          userName={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-        />
-      </section>
-      <section></section>
+      <Profile
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics
+        title="Upload stats"
+        stats={stats}
+      />
     </>
   );
 };
